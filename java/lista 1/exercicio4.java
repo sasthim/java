49fac0e51f5f189d0ebbd75d3e09ed53a49cbc8b
Sas <edumantunes2509@gmail.com>
2023-08-25 15:39:59 -0300

    ex4 done

---
+++ java/lista 1/exercicio4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class exercicio4 {
+    public static void main(String[] args) {
+        double media;
+        int input, temp = 0, qtt = 0;
+        Scanner scan = new Scanner(System.in);
+        do {
+            System.out.println(
+                    "Insira os numeros para calcular a media, apenas numeros pares serao calculados (0 para terminar):");
+            input = scan.nextInt();
+
+            if (input == 0)
+                break;
+            else {
+                if (input % 2 == 0) {
+                    temp += input;
+                    qtt++;
+                }
+            }
+        } while (true);
+
+        scan.close();
+        media = (double) temp / qtt;
+        System.out.printf("Media = %.2f%n", media);
+    }
+}
