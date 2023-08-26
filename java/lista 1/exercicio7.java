7d0f53b1b33c9e4306d62269418792b0d20c8a07
Sas <edumantunes2509@gmail.com>
2023-08-26 20:25:41 -0300

    ex7 wip

--- java/lista 1/exercicio7.java
+++ java/lista 1/exercicio7.java
@@ -5,6 +5,25 @@
        médias. A entrada de dados deve terminar quando for lido um código de produto negativo.
  */
 
-public class exercicio7 {
+import java.util.Scanner;
+
+public class exercicio7 
+{
+    public static int[] cadastrar()
+    {
+        Scanner scan = new Scanner(System.in);
+        int[] produto = new int[2];
+
+        System.out.println("Digite o codigo do produto:");
+        produto[0] = scan.nextInt();
+        System.out.println("Digite o preco do produto:");
+        produto[1] = scan.nextInt();
+
+        return produto;
+    }
     
+    public static void main (String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+    }
 }
